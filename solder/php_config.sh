263125e2c6f06a1ec5ed2532f2e266de6dffef67
Leopoldo Parra <lparra.dev@gmail.com>
2020-01-03 05:28:59 -0800

    use the created user, for this we need to send the log to console

--- solder/php_config.sh
+++ solder/php_config.sh
@@ -1,3 +1,4 @@
+sed -i.bak "s|;error_log\s*=\s*log/php7/error.log|error_log = /proc/self/fd/2|g" /etc/php7/php-fpm.conf
 sed -i.bak "s|user\s*=\s*nobody|user = www-data|g" /etc/php7/php-fpm.d/www.conf
 sed -i.bak "s|group\s*=\s*nobody|group = www-data|g" /etc/php7/php-fpm.d/www.conf
 sed -i.bak "s|;*listen\s*=\s*127.0.0.1:9000|listen = 0.0.0.0:9000|g" /etc/php7/php-fpm.d/www.conf
